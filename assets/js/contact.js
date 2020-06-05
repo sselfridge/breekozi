de80533109f7780be3bcdc67af36056131663b26
Sam Selfridge <Sam.selfridge@gmail.com>
2020-06-04 22:58:52 -0700

    adding files and favicon

---
+++ assets/js/contact.js
@@ -0,0 +1,5 @@
+// setInterval(() => {
+//   const iframe = document.getElementsByTagName("iframe")[0];
+//   const doc = iframe.contentDocument || iframe.contentWindow.document;
+//   console.log(iframe);
+// }, 1000);
