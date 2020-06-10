c8b28ad7ae1f82cb19a23bbb2715004abfdaa677
Sam Selfridge <Sam.selfridge@gmail.com>
2020-06-09 18:38:04 -0700

    Add support for responsive images

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,25 @@ function clickHamburger() {
   const checkbox = document.getElementById("menuCheck");
   checkbox.checked = !checkbox.checked;
 }
+
+// https://stackoverflow.com/a/57380820/901311
+function checkWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+checkWebP(function (support) {
+  if (support) {
+    //Do what you whant =)
+    document.documentElement.classList.remove("noWebp");
+    document.documentElement.classList.add("webp");
+    console.log("work webp");
+  } else {
+    //Do what you whant =)
+    console.log("not work, use jgp/png");
+  }
+});
